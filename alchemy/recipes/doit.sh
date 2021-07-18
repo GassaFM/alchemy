c0d260a1a388a9a849330562066392e686c9981b
Ivan Kazmenko <gassafm@gmail.com>
2021-07-18 14:22:20 +0300

    fixed recipes.csv refresh

--- alchemy/recipes/doit.sh
+++ alchemy/recipes/doit.sh
@@ -4,5 +4,7 @@ cd "${0%/*}"
 source ./getter-binary.sh nftelements uint64 || exit 1
 ./show-recipes || exit 1
 ../create/do-create > create.html || exit 1
-mv create.html ../../public_html/alchemy/create/ || exit 1
+../create/do-recipes-js > recipes.js || exit 1
+../create/do-create-ng > create-ng.html || exit 1
+mv create.html create-ng.html recipes.js ../../public_html/alchemy/create/ || exit 1
 mv *.html *.csv ../../public_html/alchemy/ || exit 1
